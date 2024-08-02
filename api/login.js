fef0bbd72ffd9a33e0d5a30bcac68b88a0e6a46b
Systemizable <202311768@ua.edu.lb>
2024-08-02 21:56:40 +0300

    Fix

--- api/login.js
+++ api/login.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
-const User = require('../models/User');
+const User = require('../src/models/User');
 const express = require('express');
 const app = express();
 app.use(express.json());
