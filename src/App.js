9d7a573cc842b8b528be45aac3782f6ce377503d
Systemizable <202311768@ua.edu.lb>
2024-08-02 22:09:09 +0300

    Fix

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Login from './Login';
 import SignUp from './SignUp';
 
 function App() {
     return (
-        <Router>
+        <div className="App">
             <Routes>
-                <Route path="/" element={<Login />} />
+                <Route path="/login" element={<Login />} />
                 <Route path="/signup" element={<SignUp />} />
             </Routes>
-        </Router>
+        </div>
     );
 }
 
