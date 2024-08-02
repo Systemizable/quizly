f62ead1dbe7617bde0ff48c0ccd31b66b88feaa0
Systemizable <202311768@ua.edu.lb>
2024-08-02 19:53:25 +0300

    fixes

--- src/server.js
+++ src/server.js
@@ -2,36 +2,32 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
 const app = express();
 
-// Use the port provided by the environment or default to 3001
 const PORT = process.env.PORT || 3001;
+const mongoURI = process.env.MONGODB_URI;
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// MongoDB URI from environment variables
-const mongoURI = process.env.MONGODB_URI;
-
+// Connect to MongoDB
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => {
-        console.log('Connected to MongoDB');
-    })
-    .catch((error) => {
-        console.error('Error connecting to MongoDB', error);
-    });
+    .then(() => console.log('Connected to MongoDB'))
+    .catch((error) => console.error('Error connecting to MongoDB', error));
 
-// Test route
-app.get('/', (req, res) => {
-    res.send('Hello from Express!');
-});
-
-// Routes
+// API routes
 const userRoutes = require('./routes/api');
 app.use('/api', userRoutes);
 
-// Start the server
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '..', 'build')));
+
+// The "catchall" handler: for any request that doesn't match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
