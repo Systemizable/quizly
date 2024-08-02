721ed6159db5d17a93a6466cb60150478fc9696a
Systemizable <202311768@ua.edu.lb>
2024-08-02 19:16:47 +0300

    fix

--- src/server.js
+++ src/server.js
@@ -8,8 +8,7 @@ const PORT = process.env.PORT || 3001;
 app.use(cors());
 app.use(express.json());
 
-// Connect to MongoDB Atlas
-const mongoURI = process.env.MONGODB_URI || 'mongodb+srv://josephssfeir:vCwld5yJlTHo5CF6@quizlycluster.ifysxxt.mongodb.net/mydatabase?retryWrites=true&w=majority';
+const mongoURI = process.env.MONGODB_URI;
 
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
