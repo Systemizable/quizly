1bbfb26a2417a0a3290d7dd60620e4f6f2430b8a
Systemizable <202311768@ua.edu.lb>
2024-08-04 13:03:37 +0300

    Hopefully this is fixed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './Login';
-import Signup from './Signup';
+import Signup from './SignUp';
 
 const App = () => {
     return (
